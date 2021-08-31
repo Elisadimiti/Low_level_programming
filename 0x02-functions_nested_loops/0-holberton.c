832c63e42fe2bc97771d3f8fd1dc932460fb06c8
elisadimiti <elisadimiti5@gmail.com>
2021-08-31 12:41:44 +0300

    A program that prints Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -5,17 +5,17 @@
  *
  * Return: 0 (success)
  **/
- int main(void)
- {
-	 _putchar('H');
-	 _putchar('o');
-	 _putchar('l');
-	 _putchar('b');
-	 _putchar('e');
-	 _putchar('r');
-	 _putchar('t');
-	 _putchar('o');
-	 _putchar('n');
-	 _putchar('\n');
-	 return (0);
- }
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
