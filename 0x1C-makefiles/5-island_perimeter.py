322ce2e1f4b31161b14794151f773352b3f0ab73
elisadimiti <elisadimiti5@gmail.com>
2022-02-09 17:26:43 +0300

    Makefile files executable

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+5-island_perimeter.py
+"""
+
+
+def island_perimeter(grid)
+
+count = 0
+for i in range(0, len(grid)):
+    for j in rang(0, len(grid[i])):
+        if grid[i][j] == 1:
+            count += 4
+            if i > 0 and grid[i - 1][j] == 1:
+                count -= 1
+            if i < len(grid) - 1 and grid[i + 1][j] == 1:
+                count -= 1
+            if j > 0 and grid[i][j - 1] == 1:
+                count -= 1
+            if j < len(grid[i]) - 1 and grid[i][j + 1] == 1:
+                count -= 1
+    return count
